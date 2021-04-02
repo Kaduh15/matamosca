4bfa639366c2b6aa4565afd6530d000927240f31
Kaduh15 <kadu.silva2014@gmail.com>
2021-04-02 18:15:30 -0300

    aleatorisa a posção da mosca na tela

--- jogo.js
+++ jogo.js
@@ -9,7 +9,19 @@ function ajustaTamanhoPalcoJogo(){
     console.log(altura, largura)
 }
 
-var posiçãoX = parseInt(Math.random() * largura)
-var posiçãoY = parseInt(Math.random() * altura)
+var posiçãoX = parseInt(Math.random() * largura) - 90
+var posiçãoY = parseInt(Math.random() * altura) - 90
+
+posiçãoX = posiçãoX < 0 ? 0 : posiçãoX 
+posiçãoY = posiçãoY < 0 ? 0 : posiçãoY 
 
 console.log(posiçãoX, posiçãoY)
+
+var mosca = document.createElement("img")
+mosca.src = "Img/mosca.png"
+mosca.className = "mosca"
+mosca.style.left = posiçãoX + "px"
+mosca.style.top = posiçãoY + "px"
+mosca.style.position = "absolute"
+
+document.body.appendChild(mosca)
