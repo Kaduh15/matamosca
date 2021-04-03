d7d81d3a5c32c0608c0ebcbc99006455fa2aefab
Kaduh15 <kadu.silva2014@gmail.com>
2021-04-02 22:24:42 -0300

    Cria função de tamanhoAleatorio

--- jogo.js
+++ jogo.js
@@ -3,25 +3,42 @@ var largura = 0
 
 ajustaTamanhoPalcoJogo()
 
-function ajustaTamanhoPalcoJogo(){
+posiçõesAleatorias()
+
+function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
     console.log(altura, largura)
 }
 
-var posiçãoX = parseInt(Math.random() * largura) - 90
-var posiçãoY = parseInt(Math.random() * altura) - 90
+function posiçõesAleatorias() {
+    var posiçãoX = parseInt(Math.random() * largura) - 90
+    var posiçãoY = parseInt(Math.random() * altura) - 90
 
-posiçãoX = posiçãoX < 0 ? 0 : posiçãoX 
-posiçãoY = posiçãoY < 0 ? 0 : posiçãoY 
+    posiçãoX = posiçãoX < 0 ? 0 : posiçãoX
+    posiçãoY = posiçãoY < 0 ? 0 : posiçãoY
 
-console.log(posiçãoX, posiçãoY)
+    console.log(posiçãoX, posiçãoY)
 
-var mosca = document.createElement("img")
-mosca.src = "Img/mosca.png"
-mosca.className = "mosca"
-mosca.style.left = posiçãoX + "px"
-mosca.style.top = posiçãoY + "px"
-mosca.style.position = "absolute"
+    var mosca = document.createElement("img")
+    mosca.src = "Img/mosca.png"
+    mosca.className = tamanhoAleatorio()
+    mosca.style.left = posiçãoX + "px"
+    mosca.style.top = posiçãoY + "px"
+    mosca.style.position = "absolute"
 
-document.body.appendChild(mosca)
+    document.body.appendChild(mosca)
+}
+
+function tamanhoAleatorio() {
+    var classe = parseInt(Math.random() * 3)
+
+    switch(classe) {
+        case 0:
+            return 'mosca1'
+        case 1:
+            return 'mosca2'
+        case 2:
+            return 'mosca3'
+    }
+}
